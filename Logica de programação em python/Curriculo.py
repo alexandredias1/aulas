356327cc2f647d09d0d765ebbec25ed50e74f188
alexandre dias <85952047+alezinhuu@users.noreply.github.com>
2024-06-10 05:23:57 -0300

    testando

--- Logica de programação em python/Curriculo.py
+++ Logica de programação em python/Curriculo.py
@@ -8,7 +8,19 @@ email = (input("Digite seu email de contato: "))
 educaçao = input("Digite sua formação: ")
 Certificados = input("Informe seus cursos e certificados: ")
 
-with open(informacoes, 'w') as arquivo:
-    arquivo.writelines(f"Nome: {nome}\nEmail: {email}\nEducação: {educaçao}\nCursos e certificados: {Certificados}")
+arquivohtml = """<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curriculo</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>"""
 
+with open(informacoes, 'w') as arquivo:
+    arquivo.write(f"Nome: {nome}\nEmail: {email}\nEducação: {educaçao}\nCursos e certificados: {Certificados}")
     print("Informações adicionadas com sucesso.")
+    arquivo.write(arquivohtml)
