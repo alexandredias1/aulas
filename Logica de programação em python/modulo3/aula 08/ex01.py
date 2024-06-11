a605faae29ad3c14865404411d793d58f534edd7
alexandre dias <xanderio@outlook.com>
2024-06-10 21:36:57 -0300

    aula 08

---
+++ Logica de programação em python/modulo3/aula 08/ex01.py
@@ -0,0 +1,18 @@
+nome_turma = 'turma.txt'
+
+for i in range(5):
+    adicao = input("O que deseja adicionar?(Nome / Matricula / CPF / CEP / Email): ")
+    if adicao == "nome":
+        nome = input("Insira o nome do aluno(a): ")
+    elif adicao == "cpf":
+        cpf = int(input("Insira o CPF do aluno(a): "))
+    elif adicao == "matricula":
+        matricula = int(input("Insira o número de matricula: "))
+    elif adicao == "email":
+        email = input("Insira o email do aluno(a): ")
+    elif adicao == "cep":
+        cep = int(input("Insira o CEP do aluno(a): "))
+
+with open(nome_turma, 'w') as arquivo:
+    arquivo.writelines(f"Matricula: {matricula}\nNome: {nome}\nEmail: {email}\nCPF: {cpf}\nCEP: {cep}")
+    arquivo.write(nome_turma)
